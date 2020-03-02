7ff4120d667defccfee963e5f482ff73112de5b6
Elbert Ng <ng.elbert@yahoo.com>
2020-03-02 15:06:56 -0800

    music functions kind of fixed

--- MusicFunctions.js
+++ MusicFunctions.js
@@ -5,7 +5,7 @@ const ytdl = require('ytdl-core');
 
 async function play(message, serverQueue, queue) {
     const arr = message.content.substring(12).split(' ');
-    const searchedVideo = arr[0];
+    const song = arr[0];
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) {
         message.channel.send("Kaede cannot play music if you are not in a voice channel!");
@@ -19,25 +19,25 @@ async function play(message, serverQueue, queue) {
         message.channel.send('Kaede is not allowed to speak in the voice channel!');
     }
 
-    const vidInfo = await ytdl.getInfo(searchedVideo);
-    const vid = {
-        title : vidInfo.title,
-        url : vidInfo.video_url,
+    const songInfo = await ytdl.getInfo(song);
+    const songData = {
+        title : songInfo.title,
+        url : songInfo.video_url,
     };
     if (typeof serverQueue === "undefined") {
         const queueFields = {
             textChannel : message.channel,
             voiceChannel : voiceChannel,
             connection : null,
-            videos : [],
+            songs : [],
             volume : 5,
         };
         queue.set(message.guild.id, queueFields);
-        queueFields.videos.push(vid);
+        queueFields.songs.push(songData);
         try {
             var connection = await voiceChannel.join();
             queueFields.connection = connection;
-            dispatchVideo(message, queueFields.videos[0], queue); 
+            dispatchSong(message, queueFields.songs[0], queue); 
         } catch (err) {
             console.log(err);
             queue.delete(message.guild.id);
@@ -46,23 +46,23 @@ async function play(message, serverQueue, queue) {
         }
     }
     else {
-        serverQueue.videos.push(vid);
+        serverQueue.songs.push(vid);
         message.channel.send("Kaede has added " + vid.title + " has been added to the queue!");
     }
 }
-async function dispatchVideo(message, video, queue) {
+async function dispatchSong(message, song, queue) {
     const serverQueue = queue.get(message.guild.id);
 
-    if (!video) {
+    if (!song) {
         serverQueue.voiceChannel.leave();
-        queue.delete(guild.id);
+        queue.delete(message.guild.id);
         message.channel.send("Kaede's bored.. Leaving now!");
         return;
     }
-    const dispatcher = serverQueue.connection.playStream(ytdl(video.url)).on('end', () => {
+    const dispatcher = serverQueue.connection.playStream(ytdl(song.url)).on('end', () => {
         message.channel.send("Kaede's song ended!");
-        serverQueue.videos.shift();
-        play(message, serverQueue.videos[0]);
+        serverQueue.songs.shift();
+        dispatchSong(message, serverQueue.songs[0], queue);
     }).on('error', () => {
         message.channel.send("Unexpected error occured!! Kaede's scared...");
         console.error(error);
