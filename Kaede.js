b0efe3d86732f2a4698394248b0d7cbc3992f38f
Jamie Sebastian <jamiesebastianjo@gmail.com>
2020-03-04 00:47:06 -0500

    disable kaede only for games during games

--- Kaede.js
+++ Kaede.js
@@ -21,7 +21,7 @@ bot.once('ready', () => {
 })
 
 bot.on('message', async message => {
-    if (!gameInProgress && message.content.substring(0, 1) === prefix && !message.author.bot) {
+    if (message.content.substring(0, 1) === prefix && !message.author.bot) {
         var firstcmd = "help"
         if (message.content.length !== 1) {
             var arr = message.content.substring(1).split(' ');
@@ -38,35 +38,37 @@ bot.on('message', async message => {
                 message.channel.send("hellooo <3");
                 break;
             case "play":
-                if (arr.length === 1) {
-                    message.channel.send(
-                        "What game do you want to play? Type `^play [gameCMD]` to play the game.\n" +
-                        ">>> Rock, paper & scissors; `= rps`\n" + 
-                        "Guess the number; `= guess`\n" + 
-                        "Answer most math questions; `= mm`\n" +
-                        "Best Game Incoming..."
-                        );
-                }
-                else {
-                    gameInProgress = true;
-                    switch (arr[1]) {
-                        case "rps":
-                            await GameFunctions.rps(message);
-                            break;
-                        case "guess":
-                            await GameFunctions.guessNumber(message);
-                            break;
-                        case "mm":
-                            await GameFunctions.mostMath(message);
-                            break;
-                        case "bjack":
-                            await GameFunctions.blackJack(message);
-                            break;
-                        default:
-                            message.channel.send("Kaede doesn't know that game!! <:illyapout:683110138235977758>");
-                            break;
+                if (!gameInProgress) {
+                    if (arr.length === 1) {
+                        message.channel.send(
+                            "What game do you want to play? Type `^play [gameCMD]` to play the game.\n" +
+                            ">>> Rock, paper & scissors; `= rps`\n" + 
+                            "Guess the number; `= guess`\n" + 
+                            "Answer most math questions; `= mm`\n" +
+                            "Best Game Incoming..."
+                            );
                     }
-                    gameInProgress = false;
+                    else {
+                        gameInProgress = true;
+                        switch (arr[1]) {
+                            case "rps":
+                                await GameFunctions.rps(message);
+                                break;
+                            case "guess":
+                                await GameFunctions.guessNumber(message);
+                                break;
+                            case "mm":
+                                await GameFunctions.mostMath(message);
+                                break;
+                            case "bjack":
+                                await GameFunctions.blackJack(message);
+                                break;
+                            default:
+                                message.channel.send("Kaede doesn't know that game!! <:illyapout:683110138235977758>");
+                                break;
+                        }
+                        gameInProgress = false;
+                    }
                 }
                 break;
             case "music":
