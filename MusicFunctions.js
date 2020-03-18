3128c74118ed5c28bd6e82c38646bb02d0382801
Elbert Ng <ng.elbert@yahoo.com>
2020-03-18 01:14:39 -0700

    rich embed for top 5 songs done

--- MusicFunctions.js
+++ MusicFunctions.js
@@ -30,20 +30,34 @@ async function play(message, serverQueue, queue) {
         message.channel.send("Kaede cannot find any songs with that title!");
         return;
     }
-    const songInfoEmbed = new Discord.RichEmbed().setColor
-    ('AQUA').setTitle('Top 5 songs').setAuthor('Kaede').setImage
-    ('https://vignette.wikia.nocookie.net/assassination-classroom/images/f/f0/Kaede_kayano_Profil.jpg/revision/latest/top-crop/width/360/height/450?cb=20160304163916&path-prefix=de')
-    .setDescription('Please type the song number').setURL('https://jamiesebastian.com/').addField(
-        {name : 'Song number 1', value : songInfo[0].title, inline: false},
-        {name : 'Song number 2', value : songInfo[1].title, inline: false},
-        {name : 'Song number 3', value : songInfo[2].title, inline: false},
-        {name : 'Song number 4', value : songInfo[3].title, inline: false},
-        {name : 'Song number 5', value : songInfo[4].title, inline: false},
-    ).setFooter('Tip: Kaede can do more cool stuff than this! Check out ^[Category CMDS] help or in general do ^help');
+    const songInfoEmbed = new Discord.RichEmbed().setColor(
+    'AQUA').setTitle('Top 5 songs').setAuthor('Kaede', message.client.user.avatarURL /* if have kaede website link put here*/).setImage(
+    'https://vignette.wikia.nocookie.net/assassination-classroom/images/f/f0/Kaede_kayano_Profil.jpg/revision/latest/top-crop/width/360/height/450?cb=20160304163916&path-prefix=de')
+    .setDescription('Kaede does not know what you want! So you choose...').addField(
+    'Song number 1', songInfo[0].title, false).addField(
+    'Song number 2', songInfo[1].title, false).addField(
+    'Song number 3', songInfo[2].title, false).addField(
+    'Song number 4', songInfo[3].title, false).addField(
+    'Song number 5', songInfo[4].title, false).setFooter(
+    'Tip: Kaede can do more cool stuff than this! Check out ^help!', 'https://www.googlecover.com/_asset/_cover/Anime-Girl-Winking_780.jpg');
+    const chooseFilter = m => {
+        let choice = Number(m.content);
+        let cond =  m.author.id === message.author.id && !isNaN(choice);
+        return cond && choice >= 1 && choice <= 5 && Number.isInteger(choice);
+    }
     message.channel.send(songInfoEmbed);
+    try {
+        let collected = await message.channel.awaitMessages(chooseFilter, {max: 1, time: 30000, errors : ['time']});
+        var index = Number(collected.first().content);
+    } catch (error){
+        message.channel.send("Kaede waited too long for this!");
+        return;
+    }
+    //console.log(songInfo);
+    //console.log(index);
     const songData = {
-        title : songInfo[0].title,
-        url : songInfo[0].link,
+        title : songInfo[index - 1].title,
+        url : songInfo[index - 1].link,
     };
     if (typeof serverQueue === "undefined") {
         const queueFields = { // queuefields is the same as serverQueue.
